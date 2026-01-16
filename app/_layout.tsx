bdd5c7d25dedaa91b49136e871172e4ddc4e1ef7
avoy <avoydas0506@gmail.com>
2026-01-16 22:25:03 +0530

    feat: added splash screen and icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,20 @@
 import { Stack } from 'expo-router';
 import './global.css';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 const rootLayout = () => {
+  const [ready,setReady] = useState(false);
+  useEffect(() => {
+    setTimeout(async () => {
+      await SplashScreen.hideAsync();
+      setReady(true);
+    }, 3000)
+  }, [])
+
+  if(!ready) return null;
   return (
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false}}/>
