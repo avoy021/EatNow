b1097fc4e594e157331efe59375f0d7220f23022
avoy <avoydas0506@gmail.com>
2026-01-19 15:08:08 +0530

    Update FoodCategories component

--- components/FoodCategories.tsx
+++ components/FoodCategories.tsx
@@ -1,10 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
+import categories from "@/assets/data/categories";
 
 const FoodCategories = () => {
   return (
-    <View>
-      <Text>FoodCategories</Text>
+    <View className="">
+      <FlatList
+        data={categories}
+        renderItem={({ item }) => <CategoryItem Item={item} />}
+        horizontal
+      />
+    </View>
+  );
+};
+
+const CategoryItem = ({ Item }: any) => {
+  return (
+    <View className="flex-row justify-center items-center w-14 h-14 mr-8 shadow-md rounded-xl focus:bg-orange-400">
+      <img src={Item.img} alt={Item.title} className="w-8 h-8 rounded-full" />
+      <Text>{Item.title}</Text>
     </View>
   );
 };
